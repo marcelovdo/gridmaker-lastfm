ebf5e8888300e38ce8ed58cdb0ce616909db9a1e
Marcelo Valentim <vdomarcelo@gmail.com>
2021-11-18 16:16:01 -0300

    initial test for getweeklychart

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-// http://www.last.fm/api/auth/?api_key=xxx&cb=http://example.com
-    // <callback_url>/?token=xxxxxxx
-
-// auth.getSession
-    // api_key: Your 32-character API Key.
-    // token: The authentication token received at your callback url as a GET variable.
-    // api_sig: Your 32-character API method signature, as explained in Section 6
-
-    // sk (Required) : The session key returned by auth.getSession service.
-    // api_key (Required) : Your 32-character API key.
-    // api_sig (Required) : Your API method signature, constructed as explained in Section 6
+const getAlbums = async () => {
+    const params = {
+        method: 'user.getweeklyalbumchart',
+        user: 'marcelovalentim',
+        api_key: ,
+        format: 'json'
+    };
+    axios.get('http://ws.audioscrobbler.com/2.0/');
+};
