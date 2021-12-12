e161b90d3fedcefabaaefdf28f47d68ae90c06bd
Marcelo Valentim <vdomarcelo@gmail.com>
2021-12-12 20:47:21 -0300

    getting back previous api call feature, now in React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, {useState} from "react";
+import axios from "axios";
 import './App.css';
+
+const rootURL = 'http://ws.audioscrobbler.com/2.0/';
 
 function App() {
   const [username, setUsername] = useState("");
+  const [period, setPeriod] = useState("overall");
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handlePeriodChange = (event) => {
+    setPeriod(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`username sent: ${username}`);
+    const config = { params: {
+      method: 'user.gettopalbums',
+      user: username,
+      api_key: process.env.REACT_APP_API_KEY, // TODO: change to backend before deploy
+      period,
+      limit: 20,
+      format: 'json' 
+    }};
+    const res = await axios.get(rootURL, config);
+    for (let album of res.data.topalbums.album) {
+      console.log(`Album: ${album.name} | Artist: ${album.artist.name} | Playcount: ${album.playcount}`);
+    }
+    setUsername("");
   };
 
   return (
@@ -18,6 +38,17 @@ function App() {
         <label>
           Enter username:
           <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <label>
+          Choose period:
+          <select value={period} onChange={handlePeriodChange}>
+            <option value="overall">Overall</option>
+            <option value="7day">7 Days</option>
+            <option value="1month">1 Month</option>
+            <option value="3month">3 Months</option>
+            <option value="6month">6 Months</option>
+            <option value="12month">12 Months</option>
+          </select>
         </label>
         <input type="submit" value="send" />
     </form>
