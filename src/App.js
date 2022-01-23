d19fe70a19736555900cd57ee7b10781c929a960
Marcelo Valentim <vdomarcelo@gmail.com>
2022-01-23 14:07:36 -0300

    Adding in some styles

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Form from "./Form";
-import ImageList from "./ImageList"
-import './App.css';
+import ImageList from "./ImageList";
+import styles from "./styles/App.module.css";
 
-const rootURL = 'http://ws.audioscrobbler.com/2.0/';
+const rootURL = "http://ws.audioscrobbler.com/2.0/";
 
 function App() {
   const [imageList, setImageList] = useState([]);
 
   const updateImages = async (username, period, gridSize) => {
     const limit = parseInt(gridSize);
-    const config = { params: {
-      method: 'user.gettopalbums',
-      user: username,
-      api_key: process.env.REACT_APP_API_KEY, // TODO: change to backend before deploy
-      period,
-      limit,
-      format: 'json' 
-    }};
+    const config = {
+      params: {
+        method: "user.gettopalbums",
+        user: username,
+        api_key: process.env.REACT_APP_API_KEY, // TODO: change to backend before deploy
+        period,
+        limit,
+        format: "json",
+      },
+    };
     const res = await axios.get(rootURL, config);
     let currentImageList = [];
     for (let album of res.data.topalbums.album) {
-      currentImageList.push(album.image[3]['#text']);
+      currentImageList.push(album.image[3]["#text"]);
       //console.log(`Album: ${album.name} | Artist: ${album.artist.name} | Playcount: ${album.playcount}`);
     }
     setImageList(currentImageList);
   };
 
   return (
-    <>
+    <div className={styles.container}>
       <Form updateImages={updateImages} />
       <ImageList imageList={imageList} />
-    </>
+    </div>
   );
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,59 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import styles from "./styles/Form.module.css";
 
 function Form(props) {
-    const {updateImages} = props;
-    const [username, setUsername] = useState("");
-    const [period, setPeriod] = useState("overall");
-    const [gridSize, setGridSize] = useState("9");
+  const { updateImages } = props;
+  const [username, setUsername] = useState("");
+  const [period, setPeriod] = useState("overall");
+  const [gridSize, setGridSize] = useState("9");
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
-    
-    const handlePeriodChange = (event) => {
-        setPeriod(event.target.value);
-    };
-    
-    const handleGridSizeChange = (event) => {
-        setGridSize(event.target.value);
-    };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        updateImages(username, period, gridSize);
-        setUsername("");
-    };
+  const handlePeriodChange = (event) => {
+    setPeriod(event.target.value);
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Enter username:
-            <input type="text" value={username} onChange={handleUsernameChange} />
-          </label>
-          <label>
-            Choose period:
-            <select value={period} onChange={handlePeriodChange}>
-              <option value="overall">Overall</option>
-              <option value="7day">7 Days</option>
-              <option value="1month">1 Month</option>
-              <option value="3month">3 Months</option>
-              <option value="6month">6 Months</option>
-              <option value="12month">12 Months</option>
-            </select>
-          </label>
-          <label>
-            Choose grid:
-            <select value={gridSize} onChange={handleGridSizeChange}>
-              <option value="9">3x3</option>
-              <option value="16">4x4</option>
-              <option value="25">5x5</option>
-              <option value="49">7x7</option>
-              <option value="100">10x10</option>
-            </select>
-          </label>
-          <input type="submit" value="send" />
-      </form>
-    );
+  const handleGridSizeChange = (event) => {
+    setGridSize(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateImages(username, period, gridSize);
+    setUsername("");
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.input}>
+        <label>
+          Enter username:
+          <input
+            className={styles.box}
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </label>
+      </div>
+      <div className={styles.input}>
+        <label>
+          Choose period:
+          <select
+            className={styles.box}
+            value={period}
+            onChange={handlePeriodChange}
+          >
+            <option value="overall">Overall</option>
+            <option value="7day">7 Days</option>
+            <option value="1month">1 Month</option>
+            <option value="3month">3 Months</option>
+            <option value="6month">6 Months</option>
+            <option value="12month">12 Months</option>
+          </select>
+        </label>
+      </div>
+      <div className={styles.input}>
+        <label>
+          Choose grid:
+          <select
+            className={styles.box}
+            value={gridSize}
+            onChange={handleGridSizeChange}
+          >
+            <option value="9">3x3</option>
+            <option value="16">4x4</option>
+            <option value="25">5x5</option>
+            <option value="49">7x7</option>
+            <option value="100">10x10</option>
+          </select>
+        </label>
+      </div>
+      <input className={styles.button} type="submit" value="send" />
+    </form>
+  );
 }
 
 export default Form;
